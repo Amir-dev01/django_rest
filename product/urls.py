eeb02c281d04fa1947844bdfa68c8b894c553f30
Amir-dev01 <saliharamir1@gmail.com>
2025-04-03 15:22:25 +0600

    hw1

---
+++ product/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import CategoryList, CategoryDetail, ProductList, ProductDetail, ReviewList, ReviewDetail
+
+urlpatterns = [
+    path('api/v1/categories/', CategoryList.as_view()),
+    path('api/v1/categories/<int:id>/', CategoryDetail.as_view()),
+    path('api/v1/products/', ProductList.as_view()),
+    path('api/v1/products/<int:id>/', ProductDetail.as_view()),
+    path('api/v1/reviews/', ReviewList.as_view()),
+    path('api/v1/reviews/<int:id>/', ReviewDetail.as_view()),
+]
---
+++ product/views.py
@@ -0,0 +1,84 @@
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Product, Category,Review
+from .serializers import CategorySerializer, ProductSerializer, ReviewSerializer
+
+
+class CategoryList(APIView):
+    def get(self,request):
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
+
+    def post(self,request):
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CategoryDetail(APIView):
+    def get(self, request, id):
+        category = get_object_or_404(Category, id=id)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+
+    def delete(self, request, id):
+        category = get_object_or_404(Category, id=id)
+        category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class ProductList(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ProductDetail(APIView):
+    def get(self, request, id):
+        product = get_object_or_404(Product, id=id)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    def delete(self, request, id):
+        product = get_object_or_404(Product, id=id)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class ReviewList(APIView):
+    def get(self, request):
+        reviews = Review.objects.all()
+        serializer = ReviewSerializer(reviews, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ReviewSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ReviewDetail(APIView):
+    def get(self, request, id):
+        review = get_object_or_404(Review, id=id)
+        serializer = ReviewSerializer(review)
+        return Response(serializer.data)
+
+    def delete(self, request, id):
+        review = get_object_or_404(Review, id=id)
+        review.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
